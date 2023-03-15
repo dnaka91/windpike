61ba09ddfb7133c2af7ba659f069f9384a1a385e
Dominik Nakamura <dnaka91@gmail.com>
2023-03-15 22:16:38 +0900

    Move license to MIT

--- src/operations/bitwise.rs
+++ src/operations/bitwise.rs
@@ -1,17 +1,3 @@
-// Copyright 2015-2020 Aerospike, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 //! Bit operations. Create bit operations used by client operate command.
 //! Offset orientation is left-to-right.  Negative offsets are supported.
 //! If the offset is negative, the offset starts backwards from end of the bitmap.
