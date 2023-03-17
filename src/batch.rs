efd69f37525464bd336d34cd755a196f1733c81a
Dominik Nakamura <dnaka91@gmail.com>
2023-03-17 22:32:27 +0900

    Combine batch modules into single file

--- src/batch.rs
+++ src/batch.rs
@@ -3,12 +3,11 @@ use std::{cmp, collections::HashMap, sync::Arc};
 use tokio::sync::Mutex;
 
 use crate::{
-    batch::BatchRead,
     cluster::{partition::Partition, Cluster, Node},
     commands::BatchReadCommand,
     errors::{Error, Result},
     policy::{BatchPolicy, Concurrency},
-    Key,
+    Bins, Key, Record,
 };
 
 pub struct BatchExecutor {
@@ -106,5 +105,39 @@ impl BatchExecutor {
     async fn node_for_key(&self, key: &Key) -> Option<Arc<Node>> {
         let partition = Partition::new_by_key(key);
         self.cluster.get_node(&partition).await
+    }
+}
+
+/// Key and bin names used in batch read commands where variable bins are needed for each key.
+#[derive(Clone, Debug)]
+pub struct BatchRead {
+    /// Key.
+    pub key: Key,
+
+    /// Bins to retrieve for this key.
+    pub bins: Bins,
+
+    /// Will contain the record after the batch read operation.
+    pub record: Option<Record>,
+}
+
+impl BatchRead {
+    /// Create a new `BatchRead` instance for the given key and bin selector.
+    #[must_use]
+    pub const fn new(key: Key, bins: Bins) -> Self {
+        Self {
+            key,
+            bins,
+            record: None,
+        }
+    }
+
+    #[must_use]
+    pub(crate) fn match_header(&self, other: &Self, match_set: bool) -> bool {
+        let key = &self.key;
+        let other_key = &other.key;
+        (key.namespace == other_key.namespace)
+            && (match_set && (key.set_name == other_key.set_name))
+            && (self.bins == other.bins)
     }
 }
--- src/batch/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod batch_executor;
-pub mod batch_read;
-
-pub use self::{batch_executor::BatchExecutor, batch_read::BatchRead};
