80c4ed260914282e3d93b9e03df7bdfd68315565
Dominik Nakamura <dnaka91@gmail.com>
2023-07-07 23:01:14 +0900

    Optimize macros and update usage examples

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,16 @@
-/// Constructs a new List Value from a list of one or more native data types.
+/// Constructs a [`Vec`]<[`Value`](crate::Value)> of from a list of native data types.
+#[macro_export]
+macro_rules! as_values {
+    ($($v:expr),* $(,)?) => {{
+        vec![$($crate::Value::from($v),)*]
+    }};
+}
+
+/// Constructs a [`Value::List`](crate::Value::List) from a list of native data types.
 ///
 /// # Examples
 ///
-/// Write a list value to a record bin.
+/// Write a list value to a record.
 ///
 /// ```rust
 /// use windpike::{
@@ -18,40 +26,32 @@
 ///         .unwrap();
 ///
 ///     let key = Key::new("test", "test", "mykey");
-///     let list = as_list!("a", "b", "c");
-///     let bin = Bin::new("list", list);
+///     let bin = Bin::new("list", as_list!("a", 1, true));
+///
 ///     client
-///         .put(&WritePolicy::default(), &key, &vec![bin])
+///         .put(&WritePolicy::default(), &key, &[bin])
 ///         .await
 ///         .unwrap();
 /// }
 /// ```
 #[macro_export]
 macro_rules! as_list {
-    ($($v:expr),*) => {{
+    ($($v:expr),* $(,)?) => {{
         $crate::Value::List(
-            vec![$($crate::Value::from($v),)*]
+            $crate::as_values!($($v,)*)
         )
     }};
 }
 
-/// Constructs a vector of Values from a list of one or more native data types.
-#[macro_export]
-macro_rules! as_values {
-    ($($v:expr),*) => {{
-        vec![$($crate::Value::from($v),)*]
-    }};
-}
-
-/// Constructs a Map Value from a list of key/value pairs.
+/// Constructs a [`MapKey`](crate::MapKey) from a list of key/value pairs.
 ///
 /// # Examples
 ///
-/// Write a map value to a record bin.
+/// Write a map value to a record.
 ///
 /// ```rust
 /// use windpike::{Bin, Key, as_map, Client, policy::ClientPolicy, policy::WritePolicy};
-
+///
 /// #[tokio::main]
 /// async fn main() {
 ///     let client = Client::new(&ClientPolicy::default(), "localhost:3000")
@@ -59,21 +59,19 @@ macro_rules! as_values {
 ///         .unwrap();
 ///
 ///     let key = Key::new("test", "test", "mykey");
-///     let map = as_map!("a" => 1, "b" => 2);
-///     let bin = Bin::new("map", map);
+///     let bin = Bin::new("map", as_map!("a" => true, 2 => 10.0));
+///
 ///     client
-///         .put(&WritePolicy::default(), &key, &vec![bin])
+///         .put(&WritePolicy::default(), &key, &[bin])
 ///         .await
 ///         .unwrap();
 /// }
 /// ```
 #[macro_export]
 macro_rules! as_map {
-    ($($k:expr => $v:expr),*) => {{
+    ($($k:expr => $v:expr),* $(,)?) => {{
         $crate::Value::HashMap(
-            std::collections::HashMap::from([
-                $(($crate::MapKey::from($k), $crate::Value::from($v)),)*
-            ])
+            [$(($crate::MapKey::from($k), $crate::Value::from($v)),)*].into()
         )
     }};
 }
